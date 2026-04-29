432b3d7d843c3a8c66e9d20075672e5d5d11bf59
ErasmoAA <erasmoaliagapsico@gmail.com>
2026-04-28 22:17:50 -0500

    Moneytags head y js
    Moneytags head y js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10939761
+    "zoneId": 10939840
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
